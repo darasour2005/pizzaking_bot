ffacc29b3f1aabeaad57f2b46b5bfbd3219e7b53
darasour2005 <darasour2005@gmail.com>
2026-04-08 02:35:01 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE_NAME = 'pizza-king-v56';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'logo.png',
-  'https://telegram.org/js/telegram-web-app.js',
-  'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
-];
+const CACHE_NAME = 'pizza-king-v61';
+const ASSETS = ['index.html', 'manifest.json', 'logo.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
