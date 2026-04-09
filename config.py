0670c3b6607d70b73331c0e1f8b79bd0e48f6b03
darasour2005 <darasour2005@gmail.com>
2026-04-09 00:33:41 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py - MASTER BACKEND CONFIGURATION
+
+# Telegram Settings
+TELEGRAM_API_TOKEN = '8503376154:AAGsDQEaLHCq3E_ttoCAT46SygrD2VubP-E' 
+GROUP_CHAT_ID = '-1003499575831' 
+
+# WooCommerce Settings
+WC_URL = "https://1.phsar.me"
+WC_KEY = "ck_6a9c8caa18a2b0ab114ef90bb9e982d69521ec03"
+WC_SECRET = "cs_63c256e1b4eba0a65723f054159e55d2148c3c57"
+
+# Frontend PWA URL
+MINI_APP_URL = "https://darasour2005.github.io/pizzaking_bot/"
